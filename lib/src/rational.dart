2fb1bcca8aa330ecc6025fdd78a7141f0529bfd0
Chia <12760908+zdirnecamlcs96@users.noreply.github.com>
2024-11-14 21:56:07 +0800

    wip: rational number

--- lib/src/rational.dart
+++ lib/src/rational.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+
+final _pattern = RegExp(r'^([+-]?\d*)(\.\d*)?([eE][+-]?\d+)?$');
+
 class RationalNumber {
   final int numerator;
   final int denominator;
@@ -13,6 +17,163 @@ class RationalNumber {
       return a;
     }
     return _gcd(b, a % b);
+  }
+
+  static RationalNumber zero() {
+    return RationalNumber(0, 1);
+  }
+
+  RationalNumber simplify() {
+    final gcd = _gcd(numerator, denominator);
+    return RationalNumber(numerator ~/ gcd, denominator ~/ gcd);
+  }
+
+  static RationalNumber fromDecimal(String number, int precision) {
+    final regexMatch = _pattern.firstMatch(number);
+
+    if (regexMatch == null) {
+      throw ArgumentError('Invalid number format');
+    }
+
+    final integerDigits = regexMatch.group(1);
+
+    if (integerDigits == null) {
+      throw ArgumentError('Invalid number format');
+    }
+
+    var totalDigits = integerDigits.length;
+
+    final floatingPointDigits = regexMatch.group(2);
+
+    if (floatingPointDigits != null) {
+      totalDigits += floatingPointDigits.substring(1).length;
+    }
+
+    final exponentDigits = regexMatch.group(3);
+
+    if (exponentDigits != null) {
+      var exponent = int.parse(exponentDigits.substring(1));
+      if (floatingPointDigits != null) {
+        exponent -= floatingPointDigits.substring(1).length;
+      }
+      exponent -= precision;
+
+      if (exponent < 0 && exponent.abs() > totalDigits) {
+        return zero();
+      }
+    }
+
+    return RationalNumber.parse(number);
+  }
+
+  static RationalNumber parse(String value) {
+    final regexMatch = _pattern.firstMatch(value.toString());
+
+    if (regexMatch == null) {
+      throw ArgumentError('Invalid number format');
+    }
+
+    final integerDigits = regexMatch.group(1);
+
+    if (integerDigits == null) {
+      throw ArgumentError('Invalid number format');
+    }
+
+    final floatingPointDigits = regexMatch.group(2);
+    final exponentDigits = regexMatch.group(3);
+
+    int numerator = 0;
+    int denominator = 1;
+
+    if (floatingPointDigits != null) {
+      for (var i = 1; i < floatingPointDigits.length; i++) {
+        denominator = denominator * 10;
+      }
+      numerator =
+          int.parse("$integerDigits${floatingPointDigits.substring(1)}");
+    } else {
+      numerator = int.parse(integerDigits);
+    }
+
+    if (exponentDigits != null) {
+      final exponent = int.parse(exponentDigits.substring(1));
+
+      // /// pow(10, 19) = -8446744073709551616
+      // /// pow(10, 20) = 7766279631452241920
+      if (exponent > 0) {
+        String numeratorString = numerator.toString();
+        for (var i = 0; i < exponent; i++) {
+          numeratorString += '0';
+        }
+        try {
+          numerator = int.parse(numeratorString);
+        } catch (e) {
+          if (e is FormatException) {
+            /// reduce the exponent to 18
+            final powerOfTen = (log(denominator) / log(10)).floor() + exponent;
+            final diffPrecision = powerOfTen - 18;
+
+            String denominatorString = denominator.toString();
+
+            for (var i = 0; i < diffPrecision; i++) {
+              numeratorString =
+                  numeratorString.substring(0, numeratorString.length - 1);
+              denominatorString = denominatorString.substring(
+                  0, denominator.toString().length - 1);
+            }
+
+            numerator = int.parse(numeratorString);
+            denominator = int.parse(denominatorString);
+          }
+        }
+      } else {
+        denominator *= pow(10, -exponent).toInt();
+      }
+    }
+
+    if (numerator == denominator) {
+      return RationalNumber(1, 1);
+    }
+
+    return RationalNumber(numerator, denominator)._removeTrailingZeros();
+  }
+
+  /// numerator - 10
+  /// denominator - 10
+  RationalNumber _removeTrailingZeros() {
+    int countTrailingZeros(int number) {
+      int count = 0;
+
+      while (number % 10 == 0) {
+        count++;
+        number ~/= 10; // Use integer division to remove the last digit
+      }
+
+      return count;
+    }
+
+    final numeratorZeros = countTrailingZeros(numerator);
+    final denominatorZeros = countTrailingZeros(denominator);
+
+    final zeros = min(numeratorZeros, denominatorZeros);
+
+    int newNumerator = numerator;
+    int newDenominator = denominator;
+
+    for (var i = 0; i < zeros; i++) {
+      newNumerator ~/= 10;
+      newDenominator ~/= 10;
+    }
+
+    return RationalNumber(newNumerator, newDenominator);
+  }
+
+  static RationalNumber? tryParse(String value) {
+    try {
+      return parse(value);
+    } catch (e) {
+      return null;
+    }
   }
 
   RationalNumber operator +(RationalNumber other) {
--- test/rational_test.dart
+++ test/rational_test.dart
@@ -1,20 +1,68 @@
+import 'dart:math';
+
 import 'package:test/test.dart';
 import 'package:dart_decimal/src/rational.dart';
 
 void main() {
-  test('stress test', () {
-    final start = 0.001;
-    final end = 10;
+  // test('stress test', () {
+  //   final start = 0.001;
+  //   final end = 10;
 
-    for (num i = start; i <= end; i++) {
-      for (num j = start; j <= end; j++) {
-        final rational1 = RationalNumber.parse(i);
-        final rational2 = RationalNumber.parse(j);
-        final result = rational1 + rational2;
-        print('$i + $j = ${result.numerator}');
-        expect(result.numerator, equals(i + j));
-        expect(result.denominator, equals(1));
-      }
-    }
+  //   for (num i = start; i <= end; i++) {
+  //     for (num j = start; j <= end; j++) {
+  //       final rational1 = RationalNumber.parse(i);
+  //       final rational2 = RationalNumber.parse(j);
+  //       final result = rational1 + rational2;
+  //       print('$i + $j = ${result.numerator}');
+  //       expect(result.numerator, equals(i + j));
+  //       expect(result.denominator, equals(1));
+  //     }
+  //   }
+  // });
+
+  group("scientific notation", () {
+    test("scientific notation", () {
+      /// 15 fractional digits
+      /// 24 exponent digits
+      String a = "9.223372036854776e+24";
+      expect(
+        () => RationalNumber.parse(a).numerator,
+        throwsA(
+          predicate((e) =>
+              e is FormatException &&
+              e.message == 'Positive input exceeds the limit of integer'),
+        ),
+      );
+
+      String b = "1.2345e+5"; // 123450.0
+      expect(RationalNumber.parse(b).numerator, equals(123450));
+      expect(RationalNumber.parse(b).denominator, equals(1));
+
+      String c = "1.000000000000000000";
+      expect(RationalNumber.parse(c).numerator, equals(1));
+      expect(RationalNumber.parse(c).denominator, equals(1));
+
+      String d = "1.000050000000000000";
+      expect(RationalNumber.parse(d).numerator, equals(100005));
+      expect(RationalNumber.parse(d).denominator, equals(pow(10, 5)));
+    });
+
+    test("scientific notation with precision", () {
+      String amount = "9.223372036854776e+24";
+      int precision = 50;
+
+      /// 9.223372036854776e+24
+      /// = (9223372036854776 * 10^-15) * 10^24
+      /// = 9223372036854776 × 10^(-15+24)
+      /// = 9223372036854776 × 10^9
+
+      /// 9223372036854776 × 10^9 / 10^50
+      /// = 9223372036854776 × 10^(9-50)
+      /// = 9223372036854776 / 10^-41
+      final rational = RationalNumber.fromDecimal(amount, precision);
+
+      expect(rational.numerator, equals(0));
+      expect(rational.denominator, equals(1));
+    });
   });
 }
